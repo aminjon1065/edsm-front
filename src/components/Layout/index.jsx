dd529cbfc4f7a452cb196d0cbd3c4e3e6fc49cf9
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-05-25 16:36:43 +0500

    0.0.1

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -10,7 +10,7 @@ const Index = () => {
                 <div className="md:pl-64 flex flex-col flex-1">
                     <main className="flex-1">
                         <div className="py-6">
-                            <div className="max-w-12xl mx-auto px-4 sm:px-6 md:px-8">
+                            <div className="max-w-full mx-auto px-4 sm:px-6 md:px-8">
                                 {/* Replace with your content */}
                                 <div className="">
                                     <Outlet/>
