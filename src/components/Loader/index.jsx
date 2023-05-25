43a829d5f2ee8e0d7089f2ab144c67ddaf9d66b2
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-05-25 16:35:17 +0500

    0.0.1

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import  './loader.css';
 const Index = () => {
     return (
-        <div className="lds-roller">
+        <div className="lds-spinner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
             <div></div>
             <div></div>
             <div></div>
