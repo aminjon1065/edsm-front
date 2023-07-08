50feb3fc63d550d2e06c6808538ad1c48911bd43
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-07-08 13:46:23 +0500

    0.0.1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {checkAuth} from "./state/slices/signIn";
